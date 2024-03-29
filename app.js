ae5d5496b2c2fd458bd7a842393b28b7249aa334
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-03-29 16:28:51 +0530

    implemented uuid for session id

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
+//to store session
+const sessions = {};
+
+// multer
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: (req, file, cb) => {
@@ -9,10 +14,21 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("home route");
+});
+
+// create sessions endpoint to upload
+app.post("/api/v1/create-session", (req, res) => {
+  const sessionId = uuidv4();
+  sessions[sessionId] = {
+    files: [],
+    result: 0,
+  };
+  res.json({ session_id: sessionId });
 });
 
 app.post("/upload", upload.single("file"), (req, res) => {
